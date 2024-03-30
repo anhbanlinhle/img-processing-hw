048b6fa0979e4fde95526f05c8a8a758a9ec0989
anhbanlinhle <vietlinhleviet@gmail.com>
2024-03-31 04:06:15 +0700

    complete img functions

--- HW1/ex1.py
+++ HW1/ex1.py
@@ -1,62 +1,45 @@
 import cv2 
 import matplotlib.pyplot as plt
 
-
 # Load an image from file as function
 def load_image(image_path):
-    """
-    Load an image from file, using OpenCV
-    """
-    pass
+    img = cv2.imread(image_path)
+    return img
 
 # Display an image as function
 def display_image(image, title="Image"):
-    """
-    Display an image using matplotlib. Rembember to use plt.show() to display the image
-    """
-    pass
-
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title(title)
+    plt.axis(False)
+    plt.show()
 
 # grayscale an image as function
 def grayscale_image(image):
-    """
-    Convert an image to grayscale. Convert the original image to a grayscale image. In a grayscale image, the pixel value of the
-    3 channels will be the same for a particular X, Y coordinate. The equation for the pixel value
-    [1] is given by:
-        p = 0.299R + 0.587G + 0.114B
-    Where the R, G, B are the values for each of the corresponding channels. We will do this by
-    creating an array called img_gray with the same shape as img
-    """
-    pass
-
+    gray_img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    return gray_img
 
 # Save an image as function
 def save_image(image, output_path):
-    """
-    Save an image to file using OpenCV
-    """
-    pass
+    cv2.imwrite(output_path, image)
 
 
 # flip an image as function 
 def flip_image(image):
-    """
-    Flip an image horizontally using OpenCV
-    """
-    pass
-
+    flipped_img = cv2.flip(image, 1)
+    return flipped_img
 
 # rotate an image as function
 def rotate_image(image, angle):
-    """
-    Rotate an image using OpenCV. The angle is in degrees
-    """
-    pass
+    (width, height) = image.shape[1::-1]
+    image_center = (width/2, height/2) 
 
+    rotate_matrix = cv2.getRotationMatrix2D(center=image_center, angle=angle, scale=1)
+    rotate_img = cv2.warpAffine(src=image, M=rotate_matrix, dsize=(width, height))
+    return rotate_img
 
 if __name__ == "__main__":
     # Load an image from file
-    img = load_image("images/uet.png")
+    img = load_image("./uet.png")
 
     # Display the image
     display_image(img, "Original Image")
@@ -68,7 +51,7 @@ if __name__ == "__main__":
     display_image(img_gray, "Grayscale Image")
 
     # Save the grayscale image
-    save_image(img_gray, "images/lena_gray.jpg")
+    save_image(img_gray, "./lena_gray.jpg")
 
     # Flip the grayscale image
     img_gray_flipped = flip_image(img_gray)
@@ -83,7 +66,7 @@ if __name__ == "__main__":
     display_image(img_gray_rotated, "Rotated Grayscale Image")
 
     # Save the rotated grayscale image
-    save_image(img_gray_rotated, "images/lena_gray_rotated.jpg")
+    save_image(img_gray_rotated, "./lena_gray_rotated.jpg")
 
     # Show the images
     plt.show() 
